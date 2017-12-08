360a0297631a1abaf365aa4485b237d7b769ad8c
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2017-12-08 12:27:25 +0100

    8th day

---
+++ A08a.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+
+def cond(x, y, oper):
+    return {
+        "==": x == y,
+        "!=": x != y,
+        '>': x > y,
+        ">=": x >= y,
+        '<': x < y,
+        "<=": x <= y
+    }.get(oper, False)
+
+def main():
+    file1 = open("08.txt", "r")
+    lines = file1.readlines()
+    D = {}
+    for c in lines:
+        c = c[:-1]
+        words = c.split(' ')
+        if words[0] not in D: D[words[0]] = 0
+        if words[4] not in D: D[words[4]] = 0
+        if cond(D[words[4]], int(words[6]), words[5]):
+            if words[1] == "inc": D[words[0]] += int(words[2])
+            else: D[words[0]] -= int(words[2])
+    file1.close()
+    print(max(D.values()))
+
+if __name__ == '__main__':
+    main()
