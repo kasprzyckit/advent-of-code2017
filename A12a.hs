8f7b1385ada8b1ae6f7f9d6ae18237b5cad72d9e
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2017-12-12 18:20:29 +0100

    12th day

--- A12a.hs
+++ A12a.hs
@@ -1,22 +1,17 @@
 import System.IO
+import Data.Tree
 import Data.Graph
 
-bulidGraph :: [[Int]] -> Graph
-bulidGraph s = a
-    where (a, b, c) = graphFromEdges $ map (\x -> (0, head x, tail x)) s
-
-pipes :: String -> Int
-pipes = length.
-    flatten.
-    head.
-    (\l g -> dfs g l) [0].
-    bulidGraph.
-    fmap (fmap read).
-    map (map (\x -> if last x == ',' then init x else x)).
-    map (\(x:y:xs) -> x:xs).
-    fmap words.
-    lines
-
 main = do
-    contents <- readFile "12.txt"
-    print $ pipes contents
+    fmap (length.
+        flatten.
+        head.
+        (\l g -> dfs g l) [0].
+        (\(a, b, c) -> a).
+        graphFromEdges.
+        map (\x -> (0, head x, tail x)).
+        map (map $ \x -> if last x == ',' then init x else x).
+        map (\(x:y:xs) -> x:xs).
+        map words.
+        lines) $ readFile "12.txt"
+    >>= print
--- A12b.hs
+++ A12b.hs
@@ -1,20 +1,14 @@
 import System.IO
 import Data.Graph
 
-bulidGraph :: [[Int]] -> Graph
-bulidGraph s = a
-    where (a, b, c) = graphFromEdges $ map (\x -> (0, head x, tail x)) s
-
-pipes :: String -> Int
-pipes = length.
-    components.
-    bulidGraph.
-    map (map read).
-    map (map $ \x -> if last x == ',' then init x else x).
-    map (\(x:y:xs) -> x:xs).
-    map words.
-    lines
-
 main = do
-    contents <- readFile "12.txt"
-    print $ pipes contents
+    fmap (length.
+        components.
+        (\(a, b, c) -> a).
+        graphFromEdges.
+        map (\x -> (0, head x, tail x)).
+        map (map $ \x -> if last x == ',' then init x else x).
+        map (\(x:y:xs) -> x:xs).
+        map words.
+        lines) $ readFile "12.txt"
+    >>= print
