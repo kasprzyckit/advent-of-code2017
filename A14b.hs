88f117fa218e86f9bc92eab8ee6551a37964f1a2
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2017-12-15 00:31:59 +0100

    14th day

--- A14b.hs
+++ A14b.hs
@@ -11,8 +11,7 @@ main = do
             union (x + y*129) ((x-1)+y*129) (union (x + y*129) (x + (y-1)*129) $ insert (x + y*129) d2) 
             else d2) d1 [0..128]) d [0..(ln-1)]) Data.IntDisjointSet.empty.
         map (map $ (==) 1).
-        map ((++) [0]).
-        map (foldl1 (++)).
+        map (foldl (++) [0]).
         map (map $ \x -> map (signum . (.&.) x) [8,4,2,1]).
         map (map $ \x -> if ord x < 90 then (ord x)-48 else (ord x)-87).
         map knot.
